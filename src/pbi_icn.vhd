a9c5e7a74a62340479e2e116d8b00425654bb760
deuskane <deuskane@gmail.com>
2025-04-02 17:50:17 +0200

    Add pbi_target_wrapper

--- src/pbi_icn.vhd
+++ src/pbi_icn.vhd
@@ -23,7 +23,7 @@ entity pci_icn is
   generic (
     NB_TARGET         : positive   := 1;          -- Number of Target Port
     TARGET_ID         : pbi_addrs_t;
-    TARGET_ADDR_WIDTH : pbi_addrs_t
+    TARGET_ADDR_WIDTH : pbi_naturals_t
     );
 
   port (
@@ -42,15 +42,31 @@ end entity pci_icn;
 
 architecture rtl of pci_icn is
 
+  signal pbi_tgts : pbi_tgts_t (0 to NB_TARGET-1)(rdata(PBI_DATA_WIDTH -1 downto 0));
+  
 begin  -- architecture rtl
 
   
-  pbi_tgt_o <= or(pbi_tgts_i);
+  pbi_tgt_o <= or(pbi_tgts);  
   
   gen_target: for tgt in pbi_inis_o'range
   generate
 
-    pbi_inis_o(tgt) <= pbi_ini_i;
+    ins_pbi_wrapper_target : entity work.pbi_wrapper_target(rtl)
+      generic map(
+        SIZE_DATA      => PBI_DATA_WIDTH ,
+        SIZE_ADDR_IP   => TARGET_ADDR_WIDTH(tgt),
+        ID             => TARGET_ID        (tgt)
+        )
+      port map(
+        clk_i          => clk_i          ,
+        cke_i          => cke_i          ,
+        arstn_i        => arst_b_i       ,
+        pbi_ini_i      => pbi_ini_i      ,
+        pbi_tgt_o      => pbi_tgts  (tgt),     
+        pbi_ini_o      => pbi_inis_o(tgt),
+        pbi_tgt_i      => pbi_tgts_i(tgt)
+        );
     
   end generate gen_target;
 
